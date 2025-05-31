be7536e0b330dc152724250843d10ec9c392906f
Kunal Mathur <kunalmathur0803@gmail.com>
2025-06-01 00:17:49 +0530

    Refactor Printsubarrays class to include a method for printing all subarrays and their count

--- Arrays/Printsubarrays.java
+++ Arrays/Printsubarrays.java
@@ -1,7 +1,25 @@
 package Arrays;
 
 public class Printsubarrays {
+    public static void fn(int[] num) {
+        int count = 0;
+        for (int i = 0; i < num.length; i++) {
+            int start = i;
+            for (int j = i; j < num.length; j++) {
+                int end = j;
+                count++;
+                for (int k = start; k <= end; k++) {
+                    System.out.print(num[k] + " ");
+                }
+                System.out.println();
+            }
+        }
+        System.out.println(count);
+    }
+
     public static void main(String[] args) {
+        int[] num = { 2, 4, 6, 8, 10 };
+        fn(num);
 
     }
 }
